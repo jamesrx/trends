2e03fc001521dbf81ac2cb587150dee45efec254
James Rose <jrose@dotdash.com>
2019-04-04 17:23:57 -0400

    refactored answerfield

--- src/components/AnswerField.jsx
+++ src/components/AnswerField.jsx
@@ -2,30 +2,109 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from '../styles/answerField.scss';
 
-const AnswerField = ({
-  type,
-  refs,
-  onKeyUp,
-  onKeyDown,
-  onFocus,
-}) => (
-  <span // eslint-disable-line jsx-a11y/no-static-element-interactions
-    contentEditable="true"
-    name="terms"
-    className={`${type} ${style.answerField}`}
-    ref={(el) => { refs[type] = el; }} // eslint-disable-line no-param-reassign
-    onKeyUp={onKeyUp}
-    onKeyDown={onKeyDown}
-    onFocus={onFocus}
-  />
-);
+class AnswerField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.hideRef = React.createRef();
+  }
+
+  componentDidMount = () => {
+    const {
+      type,
+      refs,
+    } = this.props;
+
+    this.ref = refs[type];
+  }
+
+  resizeInput = () => {
+    this.ref.style.width = `${this.hideRef.current.offsetWidth}px`;
+  }
+
+  validateAnswer = () => {
+    const { value } = this.state;
+    const isValid = value.length < 3 || value.length > 20;
+    const { updateValidFields } = this.props;
+
+    updateValidFields(isValid);
+  }
+
+  changeHandler = (event) => {
+    const filteredInput = event.target.value.replace(/\s/g, '');
+
+    this.setState({
+      value: filteredInput,
+    }, () => {
+      this.resizeInput();
+      this.validateAnswer();
+    });
+  }
+
+  toggleDisabledInput = () => {
+    const {
+      refs,
+      type,
+    } = this.props;
+    const oppositeInput = refs[type === 'before' ? 'after' : 'before'];
+
+    this.ref.classList.remove(style.disabled);
+    oppositeInput.classList.add(style.disabled);
+
+    // re-validate the field gaining focus
+    this.validateAnswer();
+  }
+
+  render() {
+    const {
+      type,
+      refs,
+      disabled,
+    } = this.props;
+    const {
+      value,
+    } = this.state;
+    const hideStyle = {
+      position: 'absolute',
+      height: 0,
+      overflow: 'hidden',
+      whiteSpace: 'pre',
+    };
+
+    return (
+      <>
+        <span
+          style={hideStyle}
+          ref={this.hideRef}
+        >
+          {value}
+        </span>
+
+        <input
+          name="terms"
+          value={value}
+          disabled={disabled ? 'disabled' : ''}
+          className={`${type} ${style.answerField}`}
+          ref={(el) => { refs[type] = el; }} // eslint-disable-line no-param-reassign
+          onChange={this.changeHandler}
+          onFocus={this.toggleDisabledInput}
+        />
+      </>
+    );
+  }
+}
 
 AnswerField.propTypes = {
   type: PropTypes.string.isRequired,
   refs: PropTypes.object.isRequired,
-  onKeyUp: PropTypes.func.isRequired,
-  onKeyDown: PropTypes.func.isRequired,
-  onFocus: PropTypes.func.isRequired,
+  updateValidFields: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+AnswerField.defaultProps = {
+  disabled: false,
 };
 
 export default AnswerField;
--- src/components/screens/AnswerScreen.jsx
+++ src/components/screens/AnswerScreen.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import screens from '../../screenTypes';
 import AnswerField from '../AnswerField';
-// import style from '../../styles/answerScreen.scss';
 import answerFieldStyle from '../../styles/answerField.scss';
 
 class AnswerScreen extends React.Component {
@@ -94,14 +93,6 @@ class AnswerScreen extends React.Component {
       state,
     } = this.props;
 
-    // remove focus so user can't type in answer field after submitting
-    document.activeElement.blur();
-
-    // disable all answer fields
-    Object.keys(this.termRefs).forEach((ref) => {
-      this.termRefs[ref].classList.add(answerFieldStyle.disabled);
-    });
-
     this.setState({
       submittedAnswer: true,
       duplicateAnswer: false,
@@ -117,50 +108,20 @@ class AnswerScreen extends React.Component {
     );
   }
 
-  submitAnswer = (event) => {
+  submitHandler = (event) => {
     event.preventDefault();
 
     const activeInputName = this.termRefs.before.classList.contains(answerFieldStyle.disabled) ? 'after' : 'before';
-    const term = this.termRefs[activeInputName].textContent.trim();
+    const term = this.termRefs[activeInputName].value;
     const fullTerm = activeInputName === 'before' ? `${term} ${this.keyword}` : `${this.keyword} ${term}`;
 
     this.sendAnswerData(term, fullTerm);
   }
 
-  validateAnswer = (event) => {
-    if (event.which === 13) event.preventDefault();
-
-    const answerLength = event.target.innerText.trim().length;
-    const invalidAnswer = answerLength < this.minAnswerLength
-      || answerLength > this.maxAnswerLength;
-
-    this.setState({ invalidAnswer });
-
-    return !invalidAnswer;
-  }
-
-  submitAnswerOnKeyPress = (event) => {
-    if (event.which === 13 && this.validateAnswer(event)) {
-      this.submitAnswer(event);
-    }
-  }
-
-  toggleDisabledInput = (el) => {
-    const oppositeInput = this.termRefs[el.classList.contains('before') ? 'after' : 'before'];
-
-    el.classList.remove(answerFieldStyle.disabled);
-    oppositeInput.classList.add(answerFieldStyle.disabled);
-  }
-
-  focusHandler = (event) => {
-    const { submittedAnswer } = this.state;
-
-    if (submittedAnswer) {
-      document.activeElement.blur();
-    } else {
-      this.validateAnswer(event);
-      this.toggleDisabledInput(event.target);
-    }
+  updateValidFields = (isValid) => {
+    this.setState({
+      invalidAnswer: isValid,
+    });
   }
 
   render() {
@@ -182,9 +143,9 @@ class AnswerScreen extends React.Component {
 
     const answerProps = {
       refs: this.termRefs,
-      onKeyUp: this.validateAnswer,
-      onKeyDown: this.submitAnswerOnKeyPress,
-      onFocus: this.focusHandler,
+      onFocus: this.toggleDisabledInput,
+      updateValidFields: this.updateValidFields,
+      disabled: submittedAnswer,
     };
 
     return (
@@ -208,35 +169,37 @@ class AnswerScreen extends React.Component {
           </li>
         </ul>
 
-        <div>
-          <span>Your Search Term:</span>
+        <h3>Your Search Term:</h3>
+        <form
+          onSubmit={this.submitHandler}
+        >
           <AnswerField
             type="before"
             {...answerProps}
           />
 
-          {this.keyword}
+          <span>{this.keyword}</span>
 
           <AnswerField
             type="after"
             {...answerProps}
           />
-        </div>
+
+          {
+            !submittedAnswer && !invalidAnswer && (
+              <button
+                type="submit"
+                onClick={this.submitHandler}
+              >
+                Submit Answer!
+              </button>
+            )
+          }
+        </form>
 
         {invalidAnswer && (
           <p>{`Answer must be between ${this.minAnswerLength} and ${this.maxAnswerLength} characters long`}</p>
         )}
-
-        {
-          !submittedAnswer && !invalidAnswer && (
-            <button
-              type="button"
-              onClick={this.submitAnswer}
-            >
-              Submit Answer!
-            </button>
-          )
-        }
 
         {acceptedAnswer && <p>Waiting on other players to answer</p>}
         {duplicateAnswer && <p>Another player already answered with that. Try another term.</p>}
